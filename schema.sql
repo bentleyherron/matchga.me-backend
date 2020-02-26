8c35c171df3ee8d0be1a0798ee7c4e006d1ee83b
Bentley Herron <10934614+bentleyherron@users.noreply.github.com>
2020-02-26 15:36:24 -0500

    Added backend files and started on db schema and seed

---
+++ schema.sql
@@ -0,0 +1,96 @@
+create table sports (
+    id serial primary key,
+    name text
+);
+
+create table region (
+    id serial primary key,
+    city text,
+    latitude float,
+    longitude float
+);
+
+CREATE TABLE us_states (
+	id integer,
+	STATE_CODE char(2) NOT NULL,
+	STATE_NAME varchar(50) NOT NULL,
+	PRIMARY KEY (ID)
+);
+
+CREATE TABLE us_cities (
+	id integer,
+	STATE_ID integer references us_states(id),
+	CITY varchar(50) NOT NULL,
+	COUNTY varchar(50) NOT NULL,
+	LATITUDE float NOT NULL,
+	LONGITUDE float NOT NULL
+);
+
+create table users (
+    id serial primary key,
+    username text,
+    email text,
+    password text,
+    joined_date date,
+    last_logged_in date,
+    player_rating float,
+    -- favorite_sports integer references sports(id),
+    photo text
+);
+
+create table team (
+    id serial primary key,
+    name text,
+    region integer references region(id),
+    rating float,
+    photo text,
+    captain integer references users(id)
+);
+
+create table team_players (
+    player_id integer references users(id),
+    team_id integer references team(id)
+);
+
+create table favorite_sports (
+    id serial primary key,
+    sport integer references sports(id),
+    user_id integer references users(id),
+    team integer references team(id)
+);
+
+create table events (
+    id serial primary key,
+    title text,
+    region integer references region(id),
+    sport integer references sports(id),
+    longitude float,
+    latitude float,
+    winner integer references team(id),
+    date date,
+    description text,
+    photo text,
+    is_public boolean,
+    event_occured date
+);
+
+create table event_teams (
+    id serial primary key,
+    event_id integer references events(id),
+    team_id integer references team(id)
+);
+
+create table scores (
+    team_id integer references team(id),
+    event_id integer references events(id),
+    score integer
+);
+
+create table challenges (
+    team_from integer references team(id),
+    team_to integer references team(id),
+    datetime date,
+    message text,
+    wager integer,
+    is_accepted boolean
+);
