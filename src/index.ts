8872e8cd79f967d750fe43ab050319525619d621
Bentley Herron <10934614+bentleyherron@users.noreply.github.com>
2020-03-03 15:17:53 -0500

    Adds limit for json

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ const PORT: number = parseInt(process.env.PORT as string, 10);
 const app = express();
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '1mb'}));
 
 // Routing
 app.use("/users", usersRouter);
